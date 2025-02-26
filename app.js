ed45c5b4cac075f9409f236542795bd1c2efe606
Angel Salazzar <salazarlevano12@gmail.com>
2025-02-25 23:44:53 -0500

    Funcion Actualizar lita de amigos y agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos = []
+
+function actualizarListaAmigos(){
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    amigos.forEach(amigo =>{
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+} 
+
+
+function agregarAmigo(){
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === ''){
+        alert('El campo se encuentra vacio');
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = '';
+    actualizarListaAmigos();
+}
+
